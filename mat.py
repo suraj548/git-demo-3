79ca4ca7b60c3daf2319139ea31d1000bca3c298
Suraj_Gudaji <36351346+suraj548@users.noreply.github.com>
2021-09-03 08:57:32 +0530

    Initial commit of math.py

---
+++ mat.py
@@ -0,0 +1,14 @@
+#Add implementation
+def add(x,y):
+    pass
+#Subtract Implementation
+def subtract(x,y):
+    pass
+#Multiply implementation
+def multiply(x,y):
+    pass
+#Divide implementation
+def divide(x,y):
+    pass
+ 
+
